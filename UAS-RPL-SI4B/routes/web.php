af50505cb1c5ffcbf31d52c497c9e6ca1bc27655
mjoacquine <wiryajoaquin@gmail.com>
2026-05-26 15:13:46 +0700

    LARAVE;

---
+++ UAS-RPL-SI4B/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\CustomerController;
+use App\Http\Controllers\CustomerMenuController;
+use App\Http\Controllers\TransaksiController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
